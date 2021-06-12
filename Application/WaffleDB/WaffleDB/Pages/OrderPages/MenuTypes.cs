e25fc9ce42ac3c99a9059adfdcf71b780ae0711e
DennisGoss99 <dennisgossler98@gmail.com>
2021-06-12 03:54:37 +0200

    finished navMenu

---
+++ Application/WaffleDB/WaffleDB/Pages/OrderPages/MenuTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaffleDB.Pages.OrderPages
+{
+    public enum OrderTypes
+    {
+        OrderAdditions,
+        OrderPreMadeWaffles,
+        OrderSelfMadeWaffles
+    }
+}
