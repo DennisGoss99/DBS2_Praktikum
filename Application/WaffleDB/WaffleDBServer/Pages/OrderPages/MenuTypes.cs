532ed8b901306e494ce5d685de6430d58e6bf279
DennisGoss99 <dennisgossler98@gmail.com>
2021-06-18 13:49:31 +0200

    changed blazor client side -> blazor Server

---
+++ Application/WaffleDB/WaffleDBServer/Pages/OrderPages/MenuTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaffleDBServer.Pages.OrderPages
+{
+    public enum OrderTypes
+    {
+        OrderAdditions,
+        OrderPreMadeWaffles,
+        OrderSelfMadeWaffles,
+        ShoppingCart
+    }
+}
